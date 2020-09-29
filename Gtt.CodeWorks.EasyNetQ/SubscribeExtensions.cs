116a87938bc82be0f80ab69e2a6c1867379473ee
Michael Gattuso <michael.gattuso@gmail.com>
2020-09-28 22:31:11 -0400

    Add project files.

---
+++ Gtt.CodeWorks.EasyNetQ/SubscribeExtensions.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Text;
+using EasyNetQ;
+using EasyNetQ.SystemMessages;
+using EasyNetQ.Topology;
+
+namespace Gtt.CodeWorks.EasyNetQ
+{
+    public static class SubscribeExtensions
+    {
+        private const string Exceptions = "_Exceptions";
+        private const string Retry = "_Retry";
+        private const string Holding = "_Holding";
+        private const string Failed = "_Failed";
+        private const string ExchangeType = "direct";
+        private const string RetryHeader = "retry";
+
+        private static Tuple<IQueue, IExchange> CreateExceptionQueues(IBus bus, ISubscriptionResult subscription)
+        {
+            var eq = bus.Advanced.QueueDeclare($"{subscription.Queue.Name}{Exceptions}");
+            var ee = bus.Advanced.ExchangeDeclare($"{subscription.Queue.Name}{Exceptions}", ExchangeType);
+            bus.Advanced.Bind(ee, eq, "");
+            return Tuple.Create(eq, ee);
+        }
+
+        private static Tuple<IExchange, IExchange, IQueue> CreateRetryQueues(IBus bus, ISubscriptionResult subscription)
+        {
+            var re = bus.Advanced.ExchangeDeclare($"{subscription.Queue.Name}{Retry}", ExchangeType);
+            var he = bus.Advanced.ExchangeDeclare($"{subscription.Queue.Name}{Holding}", ExchangeType);
+            var fe = bus.Advanced.ExchangeDeclare($"{subscription.Queue.Name}{Failed}", ExchangeType);
+
+            var hq = bus.Advanced.QueueDeclare($"{subscription.Queue.Name}{Holding}", perQueueMessageTtl: 5000, deadLetterExchange: re.Name);
+            var fq = bus.Advanced.QueueDeclare($"{subscription.Queue.Name}{Failed}");
+
+            bus.Advanced.Bind(re, subscription.Queue, "");
+            bus.Advanced.Bind(he, hq, "");
+            bus.Advanced.Bind(fe, fq, "");
+
+            return Tuple.Create(he, fe, fq);
+        }
+
+        public static void SubscribeWithRetry<T>(this IBus bus, string subscriptionId, Action<T> handler, Action<T> failedHandler = null) where T : class
+        {
+            bus.Advanced.Conventions.ErrorExchangeNamingConvention = m => $"{m.Queue}{Exceptions}";
+            bus.Advanced.Conventions.ErrorQueueNamingConvention = m => $"{m.Queue}{Exceptions}";
+
+            //SETUP SUBSCRIBER
+            var subscription = bus.Subscribe(subscriptionId, handler);
+            var ex = CreateExceptionQueues(bus, subscription); // PROACTIVELY CREATE EXCEPTION QUEUES
+            var re = CreateRetryQueues(bus, subscription); // CREATE RETRY & HOLDING QUEUES
+
+            bus.Advanced.Consume<Error>(ex.Item1, (msg, data) =>
+            {
+                var serializer = bus.Advanced.Container.Resolve<IMessageSerializationStrategy>();
+                var retryExisted = msg.Body.BasicProperties.Headers.TryGetValue(RetryHeader, out var retryAttempt);
+                int rv = Convert.ToInt32(retryAttempt);
+                rv++;
+                if (!retryExisted)
+                {
+                    msg.Body.BasicProperties.Headers.Add(RetryHeader, rv);
+                }
+                else
+                {
+                    msg.Body.BasicProperties.Headers[RetryHeader] = rv;
+                }
+                if (rv < 5)
+                {
+                    bus.Advanced.Publish(re.Item1, "", true, msg.Body.BasicProperties, Encoding.ASCII.GetBytes(msg.Body.Message));
+                }
+                else
+                {
+                    bus.Advanced.Publish(re.Item2, "", true, msg);
+                }
+            });
+
+            if (failedHandler != null)
+            {
+                bus.Advanced.Consume<Error>(re.Item3, (msg, msgInfo) =>
+                {
+                    var serializer = bus.Advanced.Container.Resolve<IMessageSerializationStrategy>();
+                    var msgProps = msg.Body.BasicProperties;
+                    var msgBody = msg.Body.Message;
+                    var m = serializer.DeserializeMessage(msgProps, Encoding.UTF8.GetBytes(msgBody));
+                    T obj = (T)m.GetBody();
+                    failedHandler(obj);
+                });
+            }
+        }
+    }
+}
---
+++ Gtt.CodeWorks/ServiceResult.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace Gtt.CodeWorks
+{
+    /// <summary>
+    /// Service results
+    /// </summary>
+    public enum ServiceResult
+    {
+        // SUCCESSFUL STATUS
+
+        /// <summary>
+        /// Successful service result
+        /// </summary>
+        Successful,
+        /// <summary>
+        /// Successful service result and resource was created 
+        /// </summary>
+        Created,
+
+        /// <summary>
+        /// Service result was successful due to existing resource that met the criteria
+        /// </summary>
+        FulfilledByExistingResource,
+
+        // QUEUED STATUS
+
+        /// <summary>
+        /// Resource is queued up for for further processing
+        /// </summary>
+        Queued,
+
+        // BUSINESS / LOGIC ERRORS
+
+        /// <summary>
+        /// A resource expected by the request was not found
+        /// </summary>
+        ResourceNotFound,
+
+        /// <summary>
+        /// The service request resulted in a validation error due to the request details or
+        /// due to 
+        /// </summary>
+        ValidationError,
+
+        /// <summary>
+        /// The service request was not fulfilled as it conflicts with the
+        /// existing state of the service
+        /// </summary>
+        ConflictingRequest,
+        
+        /// <summary>
+        /// The request resulted in an error in the service that is expected to be
+        /// transient in nature. The called should retry the request using the same parameters.
+        /// </summary>
+        TransientError,
+
+        /// <summary>
+        /// The request resulted in a permanent error that is not expected to be recoverable in the present
+        /// state. A retry need not be attempted as subsequent attempts are not expected to result in a successful
+        /// outcome. 
+        /// </summary>
+        PermanentError,
+
+        /// <summary>
+        /// The request was denied due to a rate limiting event. The called should try again.
+        /// </summary>
+        RateLimited,
+
+        /// <summary>
+        /// The request timed out on an upstream dependent service. The current state of the upstream service
+        /// is unknown. The called should retry the request paying attention to any side-effects of the unknown status.
+        /// </summary>
+        UpstreamTimeout,
+
+        /// <summary>
+        /// The request resulted in an error in an upstream dependent service. The caller should review
+        /// the errors being returned to determine the root cause.
+        /// </summary>
+        UpstreamError
+
+    }
+}
