e2ec0b1736499cd1f4ae78d6c1a08ad01e210fa9
Michael Gattuso <michael.gattuso@gmail.com>
2020-12-21 23:19:29 -0500

    Added base client classes

---
+++ Gtt.CodeWorks/Web/CodeWorksClientBase.cs
@@ -0,0 +1,28 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Gtt.CodeWorks.Web
+{
+    public abstract class CodeWorksClientBase
+    {
+        private readonly CodeWorksClientEndpoint _endpoint;
+        private readonly HttpClient _client;
+        private readonly IHttpDataSerializer _httpDataSerializer;
+
+        protected CodeWorksClientBase(CodeWorksClientEndpoint endpoint, HttpClient client, IHttpDataSerializer httpDataSerializer)
+        {
+            _endpoint = endpoint;
+            _client = client;
+            _httpDataSerializer = httpDataSerializer;
+        }
+
+        protected Task<ServiceResponse<TResponse>> Call<TRequest, TResponse>(string serviceRoute, TRequest request) 
+            where TRequest : BaseRequest
+            where TResponse : new()
+        {
+            var converter = new HttpClientConverter(_client, _httpDataSerializer);
+            var url = _endpoint.GetUrl(serviceRoute);
+            return converter.Call<TRequest, TResponse>(request, url);
+        }
+    }
+}
---
+++ Gtt.CodeWorks/Web/CodeWorksClientEndpoint.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Gtt.CodeWorks.Web
+{
+    public abstract class CodeWorksClientEndpoint
+    {
+        private readonly Dictionary<string, string> _map;
+        private CodeWorksClientEndpoint(string rootUrl, Dictionary<string, string> urlMap)
+        {
+            if (string.IsNullOrWhiteSpace(rootUrl))
+                throw new ArgumentException("Value cannot be null or whitespace.", nameof(rootUrl));
+            Root = rootUrl;
+            _map = urlMap ?? new Dictionary<string, string>();
+        }
+
+        public string Root { get; }
+        public Uri GetUrl(string originalUrl)
+        {
+            string route = _map.GetValueOrDefault(originalUrl) ?? originalUrl;
+            var uri = new Uri(route);
+            if (uri.IsAbsoluteUri)
+            {
+                return uri;
+            }
+            return new Uri(new Uri(Root), uri);
+        }
+    }
+}
