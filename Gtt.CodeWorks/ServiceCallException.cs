0b6cf81cc3fddfeeac8024a0da2a4529001e1e17
Michael Gattuso <michael.gattuso@gmail.com>
2021-01-03 11:42:03 -0500

    Added new exception for use by easy clients

---
+++ Gtt.CodeWorks/ServiceCallException.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace Gtt.CodeWorks
+{
+    [Serializable]
+    public class ServiceCallException : Exception
+    {
+        public ResponseMetaData MetaData { get; }
+        //
+        // For guidelines regarding the creation of new exception types, see
+        //    http://msdn.microsoft.com/library/default.asp?url=/library/en-us/cpgenref/html/cpconerrorraisinghandlingguidelines.asp
+        // and
+        //    http://msdn.microsoft.com/library/default.asp?url=/library/en-us/dncscol/html/csharp07192001.asp
+        //
+
+        public ServiceCallException(ResponseMetaData metaData) : base($"Unsuccessful response received. {metaData.Result} from {metaData.ServiceName}")
+        {
+            MetaData = metaData;
+        }
+
+        protected ServiceCallException(
+            SerializationInfo info,
+            StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
