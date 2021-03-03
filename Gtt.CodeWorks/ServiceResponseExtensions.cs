a33b11655dc21cfc3c15c51ea6726caf31f951f5
Michael Gattuso <michael.gattuso@gmail.com>
2021-03-03 09:23:20 -0500

    Added response extensions

---
+++ Gtt.CodeWorks/ServiceResponseExtensions.cs
@@ -0,0 +1,18 @@
+﻿namespace Gtt.CodeWorks
+{
+    public static class ServiceResponseExtensions
+    {
+        public static bool IsSuccessful(this ServiceResponse response)
+        {
+            if (response?.MetaData == null) return false;
+
+            return response.MetaData.Result.Outcome() == ResultOutcome.Successful;
+        }
+
+        public static bool Is(this ServiceResponse response, ServiceResult result)
+        {
+            if (response?.MetaData == null) return false;
+            return response.MetaData.Result == result;
+        }
+    }
+}
